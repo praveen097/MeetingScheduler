05f028c3777c1785223e22f80d97856aef798836
praveen097 <>
2022-07-27 20:37:34 +0530

    Added strapi as backend

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2ea4cc2510990a194a5441c0885b22db'),
+    },
+  },
+});
